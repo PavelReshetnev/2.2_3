9b1e7dab0c95c9b02eb853202b979d5489cd657c
PavelReshetnev <pasha8804088@gmail.com>
2023-03-04 11:37:30 +0700

    Initial commit

---
+++ ConsoleApp3/Numbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace ConsoleApp3;
+
+public class Numbers
+{
+    public int num1;
+    public int num2;
+    
+
+    public Numbers(int num1, int num2)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    
+
+  
+    public void Sum()
+    {
+        int sum = num1 + num2;
+        Console.WriteLine($"Итоговая сумма равна {sum}\n");
+        
+    }
+
+    public void Vivod() {
+        Console.WriteLine($"Первое число: {this.num1} " + $"Второе число: {this.num2}\n");
+        
+    }
+
+    public void Edit()
+    {
+        Console.WriteLine("Редактирование... Введите свои числа");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        num2 = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Изменено ...\n");
+        
+
+    }
+
+    public void Max()
+    {
+        if (num1 > num2) {
+            Console.WriteLine($"Максимальное число - {num1}\n");
+        }else {
+            Console.WriteLine($"Максимальное число - {num2}\n");
+        }
+        
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ConsoleApp3;
+
+public class Program
+{
+    static void Main(string[] args)
+    { Numbers chisla = new Numbers(63,71);
+        
+      Console.WriteLine("Вывод всех чисел - 1\nИзменить значения - 2\nСумма всех чисел - 3\nМаксимальное значение - 4\nВыход - 5");
+      int a = Convert.ToInt32(Console.ReadLine());
+      switch (a)
+      {
+          case 1 :
+              chisla.Vivod();
+              break;
+          case 2:
+              chisla.Vivod();
+              chisla.Edit();
+              chisla.Vivod();
+              break;
+          case 3 :
+              chisla.Vivod();
+              chisla.Sum();
+              break;
+          case 4 :
+              chisla.Max();
+              break;
+          case 5:
+              break;
+      }
+      
+  }
+        
+}
+
